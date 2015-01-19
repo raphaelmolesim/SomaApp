5a2fb0d6a4e54cd2f9a19e7f0dde21a5359fd277
Raphael Molesim <raphael.molesim@lambda3.com.br>
2015-01-19 21:36:02 -0200

    First commit

---
+++ SomaApp/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SomaApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Sum(int n1, int n2)
+        {
+            var total = n1 + n2;
+            ViewBag.Message = string.Format("Total da soma: {0}", total);
+            return View();
+        }
+    }
+}
---
+++ SomaApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SomaApp.Startup))]
+namespace SomaApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
